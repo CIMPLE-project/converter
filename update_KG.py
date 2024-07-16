bf6805a3285f97fb84141354ce16da62482eb2de
Thibault Ehrhart <1208424+ehrhart@users.noreply.github.com>
2024-07-16 18:51:42 +0200

    fix: move the initialization of errors list before its first use

--- update_KG.py
+++ update_KG.py
@@ -163,6 +163,7 @@ model_con.eval()
 
 need_extract = 0
 need_convert = 0
+errors = []
 
 print('Extracting factors, entities and text from new claim reviews')
 dict_factors_entities_text = {}
@@ -230,7 +231,6 @@ for cr in cr_new:
         all_organizations_websites.append(website)
 
 
-errors = []
 
 
 print('Updating Graph')
